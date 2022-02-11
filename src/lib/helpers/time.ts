398aa76074e1f74899dfa8dc5ff4b4cfb01df235
Julian Beck <ju-fa-beck@t-online.de>
2022-02-11 18:55:49 +0100

    fix time

--- src/lib/helpers/time.ts
+++ src/lib/helpers/time.ts
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import isToday from 'dayjs/plugin/isToday';
-import weekday from 'dayjs/plugin/weekday';
-import localeData from 'dayjs/plugin/localizedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime.js';
+import isToday from 'dayjs/plugin/isToday.js';
+import weekday from 'dayjs/plugin/weekday.js';
+import localeData from 'dayjs/plugin/localizedFormat.js';
 dayjs.extend(relativeTime);
 dayjs.extend(localeData);
 dayjs.extend(isToday);
