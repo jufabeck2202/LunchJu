ca9976743bb386b2db6adc617fd806a00f4ce4e6
Julian Beck <ju-fa-beck@t-online.de>
2022-02-27 14:24:52 +0100

    Remove error

--- src/lib/components/CreateProposal.svelte
+++ src/lib/components/CreateProposal.svelte
@@ -15,7 +15,7 @@
 		isLoading = true;
 		//check if exists
 		const response = lunchSuggestions.find((meal) => meal.name === mealName.toLowerCase());
-		const error = await createLunchProposal(lunchId, mealName, selectedFood.id || response?.id);
+		const error = await createLunchProposal(lunchId, mealName, selectedFood?.id || response?.id);
 		isLoading = false;
 		if (error) {
 			alert(error.message);
@@ -45,54 +45,57 @@
 	<div class="modal" class:is-active={isProposalMenu}>
 		<div class="modal-background" />
 		<div class="modal-content">
-			<div class="card">
-				<header class="card-header">
-					<p class="card-header-title">Create Proposal</p>
-				</header>
-				<div class="card-content">
-					<div class="subtitle">What food do you want to eat?</div>
-					<div class="field">
-						<label class="label">Name</label>
-						<div class="control pb-6">
-							<AutoComplete
-								class="input"
-								type="text"
-								searchFunction={getItems}
-								bind:selectedItem={selectedFood}
-								localFiltering="false"
-								maxItemsToShowInList="3"
-								placeholder="Enter food name"
-								labelFieldName="name"
-								valueFieldName="id"
-								hideArrow="true"
-							>
-								<div slot="no-results">
-									<span>Create new Food {mealName}</span>
-								</div>
-							</AutoComplete>
+			<form on:submit|preventDefault={handleCreateLunchProposal}>
+				<div class="card">
+					<header class="card-header">
+						<p class="card-header-title">Create Proposal</p>
+					</header>
+					<div class="card-content">
+						<div class="subtitle">What food do you want to eat?</div>
+						<div class="field">
+							<label class="label">Name</label>
+							<div class="control pb-6">
+								<AutoComplete
+									class="input"
+									type="text"
+									searchFunction={getItems}
+									bind:selectedItem={selectedFood}
+									localFiltering="false"
+									maxItemsToShowInList="3"
+									placeholder="Enter food name"
+									labelFieldName="name"
+									valueFieldName="id"
+									hideArrow="true"
+								>
+									<div slot="no-results">
+										<span>Create new Food {mealName}</span>
+									</div>
+								</AutoComplete>
+							</div>
 						</div>
-					</div>
 
-					<div class="field is-grouped">
-						<div class="control">
-							<button
-								class="button is-link"
-								disabled={!selectedFood && mealName.length < 3}
-								is-loading={isLoading}
-								on:click={handleCreateLunchProposal}>Create Proposal</button
-							>
-						</div>
-						<div class="control">
-							<button
-								class="button is-link is-light"
-								on:click={() => {
-									isProposalMenu = false;
-								}}>Cancel</button
-							>
+						<div class="field is-grouped">
+							<div class="control">
+								<button
+									type="submit"
+									class="button is-link"
+									disabled={!selectedFood && mealName.length < 3}
+									is-loading={isLoading}>Create Proposal</button
+								>
+							</div>
+							<div class="control">
+								<button
+									class="button is-link is-light"
+									type="reset"
+									on:click={() => {
+										isProposalMenu = false;
+									}}>Cancel</button
+								>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</form>
 		</div>
 		<button
 			class="modal-close is-large"
